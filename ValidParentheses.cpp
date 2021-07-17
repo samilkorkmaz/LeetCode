8b04d156e360faa029f6e3f94e09120a86c606a5
Şamil Korkmaz <samil.korkmaz@gmail.com>
2021-07-17 12:33:08 +0300

    Update ValidParentheses.cpp

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -2,38 +2,30 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int normal = 0; int curly = 0; int square = 0;
         vector<char> parens;
-        try {
-            for(size_t i = 0; i < s.size(); i++) {
-                if(s[i] == '(') {normal++; parens.push_back(s[i]);}            
-                if(s[i] == ')') {
-                    if(parens.size() == 0) return false; //No opening paranthesis exists
-                    if(parens.back() == '(') {
-                        normal--; 
-                        parens.pop_back();    
-                    } else return false;
-                }
-                if(s[i] == '{') {curly++; parens.push_back(s[i]);}            
-                if(s[i] == '}') {
-                    if(parens.size() == 0) return false; //No opening paranthesis exists
-                    if(parens.back() == '{') {
-                        curly--; 
-                        parens.pop_back();    
-                    } else return false;
-                }
-                if(s[i] == '[') {square++; parens.push_back(s[i]);}            
-                if(s[i] == ']') {
-                    if(parens.size() == 0) return false; //No opening paranthesis exists
-                    if(parens.back() == '[') {
-                        square--; 
-                        parens.pop_back();    
-                    } else return false;
-                }
+        for(size_t i = 0; i < s.size(); i++) {
+            if(s[i] == '(') parens.push_back(s[i]);            
+            if(s[i] == ')') {
+                if(parens.size() == 0) return false; //No opening paranthesis exists
+                if(parens.back() == '(') {
+                    parens.pop_back();    
+                } else return false;
             }
-        } catch (...) { //happens when there is no opening paranthesis in parens vector
-            return false;
+            if(s[i] == '{') parens.push_back(s[i]);            
+            if(s[i] == '}') {
+                if(parens.size() == 0) return false; //No opening paranthesis exists
+                if(parens.back() == '{') {
+                    parens.pop_back();    
+                } else return false;
+            }
+            if(s[i] == '[') parens.push_back(s[i]);            
+            if(s[i] == ']') {
+                if(parens.size() == 0) return false; //No opening paranthesis exists
+                if(parens.back() == '[') {
+                    parens.pop_back();    
+                } else return false;
+            }
         }
-        return (normal==0) && (curly==0) && (square==0);
+        return parens.size()==0;
     }
 };
