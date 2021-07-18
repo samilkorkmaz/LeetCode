1084ec395521930f2542bdcddf9840d017fa4510
Şamil Korkmaz <samil.korkmaz@gmail.com>
2021-07-18 16:52:14 +0300

    Create ImplementstrStr.cpp

---
+++ ImplementstrStr.cpp
@@ -0,0 +1,23 @@
+//
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty()) return 0;        
+        //printf("si = %d\n", strstr(haystack.c_str(), needle.c_str())); --> returns 0 when is haystack = "aaa" and needle = "aaaa" where LeetCode expected result is 2, which is wrong!
+        int k = -1;
+        for(size_t i = 0; i<haystack.size(); i++) {
+            k = -1;
+            if(haystack[i] == needle[0]) {
+                bool isSubStr = true;
+                k = i;
+                for(size_t j = 1; j<needle.size(); j++) {                    
+                    isSubStr = isSubStr && (haystack[i+j] == needle[j]);
+                }
+                if(isSubStr) {
+                    break;
+                }
+            }
+        }
+        return k;
+    }
+};
