ae45f6093739d2fd487744bb9d96204e395d09bf
Şamil Korkmaz <samil.korkmaz@gmail.com>
2021-07-19 13:32:16 +0300

    Update PlusOne.cpp

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -12,7 +12,7 @@ public:
                 digits[i-1] = inc;
                 break;
             } else {
-                if(i == 1) {
+                if(i == 1) { //We reached left most digit while there is a carry which means left most digit was 9, so we have to add an additional zero at the end and 1 at the beginning
                     digits[i-1] = 1;  
                     digits.push_back(0);
                 } else digits[i-1] = 0;
